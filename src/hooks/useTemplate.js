46499ba3c40e2c2a9785c0da58107b93e6005dd0
ved-thakre <vedantthakre7@gmail.com>
2024-01-16 23:11:31 +0530

    added changes

--- src/hooks/useTemplate.js
+++ src/hooks/useTemplate.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query"
 import { toast } from "react-toastify";
 import { getTemplates } from "../api";
 
-
+// added changes
 const useTemplates = () => {
     const { data, isLoading, isError, refetch } = useQuery(
         "templates",
